e0de587238e4d1846c20113d8c60b7f7775ce568
Christopher Harrop <bannastre@gmail.com>
2017-05-26 13:54:07 +0100

    Switch turns and dynamically show opponent hit points

--- spec/features/hitpoint_spec.rb
+++ spec/features/hitpoint_spec.rb
@@ -2,12 +2,12 @@
 feature "Hitpoint display" do
   scenario "User can submit HP points and see them" do
     sign_in_and_play
-    expect(page).to have_content "Opponent has 50HP!"
+    expect(page).to have_content "Canard has 50HP!"
   end
 
   scenario "Player 1 can attack Player 2 and reduce P2's HP by 10" do
     sign_in_and_play
     click_button('Attack!')
-    expect(page).to have_content "Opponent has 40HP!"
+    expect(page).to have_content "Canard has 40HP!"
   end
 end
