7c59ec506bceea364631e72675a183d172e5e9a7
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-26 17:56:07 +0100

    Corrects spelling error & makes final changes

--- lib/game.rb
+++ lib/game.rb
@@ -1,30 +1,20 @@
 class Game
 
-  attr_reader :player_1, :player_2
+  attr_reader :players, :current_turn
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
-    @curent_turn = player_1
   end
 
-  def player_1
-    @players.first
-  end
-
-  def player_2
-    @players.last
-  end
-
-  def attack(opponent)
-    opponent.receive_damage
+  def attack
+    @players[1].receive_damage
     switch_turns
   end
 
   private
 
-  def switch_turns(current_turn)
-  p  @players.select { |player| player != current_turn }
-  p  @players.reject { |player| player == current_turn }
+  def switch_turns
+    players.reverse!
   end
 
 end
