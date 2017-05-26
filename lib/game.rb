d2ee6e5fc66ccccc87e481e58ca688b4ffeda1d4
Charlotte Fereday <cbrfereday@gmail.com>
2017-05-26 15:11:49 +0100

    Updates Game #switch_turns

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,10 @@
 class Game
 
-  attr_reader :player_1, :player_2, :attacker, :defender
+  attr_reader :player_1, :player_2
 
   def initialize(player_1, player_2)
     @players = [player_1, player_2]
-    @attacker = player_1
-    @defender = player_2
+    @curent_turn = player_1
   end
 
   def player_1
@@ -23,14 +22,9 @@ class Game
 
   private
 
-  def switch_turns
-    if @attacker == player_1
-      @attacker = player_2
-      @defender = player_1
-    else
-      @attacker = player_1
-      @defender = player_2
-    end
+  def switch_turns(current_turn)
+  p  @players.select { |player| player != current_turn }
+  p  @players.reject { |player| player == current_turn }
   end
 
 end
